d125d4f43958a95bf8a80c7e93b57c557b228be7
wierdbytes <wb@xff.pw>
2024-06-02 13:37:53 +0200

    [sketchybar]: fix installing procedure

--- home/dot_config/sketchybar/helpers/executable_calc_hash.sh
+++ home/dot_config/sketchybar/helpers/executable_calc_hash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find $HOME/.config/sketchybar/helpers -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256
+find $HOME/.config/sketchybar -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256
