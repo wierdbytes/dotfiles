aeb42bacd5cb5b801736edeb5ce22a4301ca528f
wierdbytes <wb@xff.pw>
2025-08-03 17:18:33 +0200

    [hammerspoon]: set temporarily correct coords for ghostty

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -1,67 +1,74 @@
 hs.ipc.cliInstall("/opt/homebrew")
 
-stackline = require "stackline"
-stackline:init()
-stackline.config:toggle('appearance.showIcons')
+-- stackline = require("stackline")
+-- stackline:init()
+-- stackline.config:toggle("appearance.showIcons")
 
 -- Switch alacritty (from https://gist.github.com/truebit/31396bb2f48c75285d724c9e9e037bcd)
 local spaces = require("hs.spaces") -- https://github.com/asmagill/hs._asm.spaces
 
-local APP_NAME = 'Ghostty'
+local APP_NAME = "Ghostty"
 -- local APP_NAME = 'Alacritty'
-local BUNDLE_ID = 'com.mitchellh.ghostty'
+local BUNDLE_ID = "com.mitchellh.ghostty"
 -- local BUNDLE_ID = 'org.alacritty'
 
 FocusWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
-  if eventType == hs.application.watcher.deactivated and appName == APP_NAME then
-    print('appName= ' .. appName .. ', eventType= ' .. eventType)
-    appObject:hide()
-  end
+	if eventType == hs.application.watcher.deactivated and appName == APP_NAME then
+		print("appName= " .. appName .. ", eventType= " .. eventType)
+		appObject:hide()
+	end
 end)
-FocusWatcher:start()
+--FocusWatcher:start()
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, 'T', function()
-  function moveWindow(alacritty, space, mainScreen)
-    -- move to main space
-    local win = nil
-    while win == nil do
-      win = alacritty:mainWindow()
-    end
-    if win:isFullScreen() then
-      hs.eventtap.keyStroke('cmd', 'return', 0, alacritty)
-    end
-    winFrame = win:frame()
-    scrFrame = mainScreen:fullFrame()
-    winFrame.w = scrFrame.w
-    winFrame.y = scrFrame.y + 24
-    winFrame.x = scrFrame.x
-    win:setFrame(winFrame, 0)
-    spaces.moveWindowToSpace(win, space)
-    if win:isFullScreen() then
-      hs.eventtap.keyStroke('cmd', 'return', 0, alacritty)
-    end
-    win:focus()
-  end
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "T", function()
+	function moveWindow(termemu, space, mainScreen)
+		-- move to main space
+		local win = nil
+		while win == nil do
+			win = termemu:mainWindow()
+		end
+		if win:isFullScreen() then
+			hs.eventtap.keyStroke("cmd", "return", 0, termemu)
+		end
+		winFrame = win:frame()
+		scrFrame = mainScreen:fullFrame()
+		-- winFrame.w = scrFrame.w - 68
+		-- winFrame.h = scrFrame.h - 120
+		winFrame.h = 1320
+		-- winFrame.y = scrFrame.y + 60
+		winFrame.y = scrFrame.y + 24
+		-- winFrame.x = scrFrame.x + 34
+		winFrame.w = scrFrame.w
+		-- winFrame.h = scrFrame.h
+		-- winFrame.y = scrFrame.y
+		winFrame.x = scrFrame.x
+		win:setFrame(winFrame, 0)
+		spaces.moveWindowToSpace(win, space)
+		if win:isFullScreen() then
+			hs.eventtap.keyStroke("cmd", "return", 0, termemu)
+		end
+		win:focus()
+	end
 
-  local alacritty = hs.application.get(BUNDLE_ID)
-  if alacritty ~= nil and alacritty:isFrontmost() then
-    alacritty:hide()
-  else
-    local space = spaces.activeSpaceOnScreen()
-    local mainScreen = hs.screen.mainScreen()
-    if alacritty == nil and hs.application.launchOrFocusByBundleID(BUNDLE_ID) then
-      local appWatcher = nil
-      appWatcher = hs.application.watcher.new(function(name, event, app)
-        if event == hs.application.watcher.launched and app:bundleID() == BUNDLE_ID then
-          app:hide()
-          moveWindow(app, space, mainScreen)
-          appWatcher:stop()
-        end
-      end)
-      appWatcher:start()
-    end
-    if alacritty ~= nil then
-      moveWindow(alacritty, space, mainScreen)
-    end
-  end
+	local terminal = hs.application.get(BUNDLE_ID)
+	if terminal ~= nil and terminal:isFrontmost() then
+		terminal:hide()
+	else
+		local space = spaces.activeSpaceOnScreen()
+		local mainScreen = hs.screen.mainScreen()
+		if terminal == nil and hs.application.launchOrFocusByBundleID(BUNDLE_ID) then
+			local appWatcher = nil
+			appWatcher = hs.application.watcher.new(function(name, event, app)
+				if event == hs.application.watcher.launched and app:bundleID() == BUNDLE_ID then
+					app:hide()
+					moveWindow(app, space, mainScreen)
+					appWatcher:stop()
+				end
+			end)
+			appWatcher:start()
+		end
+		if terminal ~= nil then
+			moveWindow(terminal, space, mainScreen)
+		end
+	end
 end)
