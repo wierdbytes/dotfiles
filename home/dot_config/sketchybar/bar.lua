ee8c897041db070a381cb2a7b5a2f4fcd06569c6
wierdbytes <wb@xff.pw>
2025-08-03 17:17:13 +0200

    [sketchybar]: bar.lua formatting

--- home/dot_config/sketchybar/bar.lua
+++ home/dot_config/sketchybar/bar.lua
@@ -3,12 +3,12 @@ local settings = require("settings")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  topmost = "window",
-  height = settings.bar.height,
-  color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
-  display = "all",
+	topmost = "window",
+	height = settings.bar.height,
+	color = colors.bar.bg,
+	padding_right = 2,
+	padding_left = 2,
+	display = "all",
 })
 
 sbar.add("event", "toggle_zen_mode")
